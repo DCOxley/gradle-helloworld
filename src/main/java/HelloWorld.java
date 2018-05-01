1ad2c91d0ab3466013543f20c145451d37a3ae15
oxley_d <dcoxley@googlemail.com>
2018-05-01 16:14:33 +0200

    fix

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("fuck you dog rat you world wo rld beater of worldsfsdgd);
+		System.out.println("fuck you dog rat you world wo rld beater of worldsfsdgd");
 	}
 }
