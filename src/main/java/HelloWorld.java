99fa8412f23d524d73b517492aba9477ea76d983
oxley_d <dcoxley@googlemail.com>
2018-05-01 16:04:40 +0200

    dog

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("fuck you cat you world wo rld beater of worldsfsdgd");
+		System.out.println("fuck you dog you world wo rld beater of worldsfsdgd");
 	}
 }
